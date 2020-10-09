2fa3469ab7fb9c64c0fd9911d50aef9c8df131c8
Daan Swart <dswart@roc-dev.com>
2020-10-09 09:32:41 +0200

    Links aangemaakt voor de alle hoofdstukken

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inhoudsopgave voor PHPVoorBeginners</title>
 
-echo 'Hallo !!!!!!!!!!!!';
+    <style>
+        #h1:hover{
+            background: yellow;
+        }
+    </style>
+</head>
+<body>
+<h1>PHP voor beginners 2020</h1>
+<h2>Daan Swart</h2>
+<h3><a id="h1" href="h01/index.php">Hoofdstuk 1</a></h3>
+<h3><a href="">Hoofdstuk 2</a></h3>
+<h3><a href="">Hoofdstuk 3</a></h3>
+<h3><a href="">Hoofdstuk 4</a></h3>
+<h3><a href="">Hoofdstuk 5</a></h3>
+<h3><a href="">Hoofdstuk 6</a></h3>
+<h3><a href="">Hoofdstuk 7</a></h3>
 
-
-?>
+</body>
+</html>
