d464dd2df14d26568ee553531af5cb86474b9dfb
Daan Swart <dswart@roc-dev.com>
2020-10-09 11:46:50 +0200

    Html met link naar index php toegevoegd.

--- h01/index.php
+++ h01/index.php
@@ -1,1 +1,19 @@
 <?php
+echo 'Hello world! First time working with php.';
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hoofdstuk 1 php</title>
+</head>
+<body>
+<br>
+<br>
+<a href="../index.php">Naar de index van PHP 2020</a>
+</body>
+</html>
