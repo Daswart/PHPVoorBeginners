083d7e47834fae9ed8c082ae0d9a1521c8b88308
Daan Swart <dswart@roc-dev.com>
2020-10-13 16:17:35 +0200

    Wat css toegevoegd aan de pagina's

--- index.php
+++ index.php
@@ -6,26 +6,23 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inhoudsopgave voor PHPVoorBeginners</title>
+    <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nova+Square&display=swap" rel="stylesheet">
 
-    <style>
-        #h1:hover{
-            background: yellow;
-        }
-        #h2:hover{
-            background: yellow;
-        }
-    </style>
 </head>
 <body>
 <h1>PHP voor beginners 2020</h1>
 <h2>Daan Swart</h2>
-<h3><a id="h1" href="h01/index.php">Hoofdstuk 1</a></h3>
-<h3><a id="h2" href="h02/index.php">Hoofdstuk 2</a></h3>
-<h3><a href="">Hoofdstuk 3</a></h3>
-<h3><a href="">Hoofdstuk 4</a></h3>
-<h3><a href="">Hoofdstuk 5</a></h3>
-<h3><a href="">Hoofdstuk 6</a></h3>
-<h3><a href="">Hoofdstuk 7</a></h3>
+<ul>
+    <li><a id="h1" href="h01/index.php">Hoofdstuk 1</a></h3></Li>
+    <li><a id="h2" href="h02/index.php">Hoofdstuk 2</a></li>
+    <li><a href="">Hoofdstuk 3</a></li>
+    <li><a href="">Hoofdstuk 4</a></li>
+    <li><a href="">Hoofdstuk 5</a></li>
+    <li><a href="">Hoofdstuk 6</a></li>
+    <li><a href="">Hoofdstuk 7</a></li>
+</ul>
+
 
 </body>
 </html>
