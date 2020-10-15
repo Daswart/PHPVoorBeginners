91779156b4903c645682327b31960474f6316ca1
Daan Swart <dswart@roc-dev.com>
2020-10-15 22:20:14 +0200

    kattenplaatjes toegevoegd voor 3.1

---
+++ h03/opdracht1.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+
+    </style>
+    <title></title>
+</head>
+<body>
+<?php
+ $katten = array('kat1.jpg', 'kat2.jpg', 'kat3.jpg', 'kat4.jpg', 'kat5.jpg', 'kat6.jpg', 'kat7.jpg', 'kat8.jpg', 'kat9.jpg');
+
+ foreach ($katten as $kat){
+    echo "<img src='../img/".$kat."'>";
+ }
+?>
+
+</body>
+</html>
---
+++ h03/opdracht3.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+    body{
+        text-align: center;
+    }
+    </style>
+    <title></title>
+</head>
+<body>
+<?php
+//Omzetten van while in for-loop
+for($x = 35; $x >=25; $x--){
+    echo $x.' hoppelepee ';
+
+
+}
+//MET DEZE CODE (CODE UIT DE OPDRACHT) GETEST HOE VAAK HOPPELEPEE WORDT GEPRINT)
+/*$i = 1;
+$x = 35;
+while ($x >= 25){
+    echo $i.'. hoppelepee';
+    echo '<br>';
+    $i++;
+    $x--;
+}*/
+?>
+
+</body>
+</html>
