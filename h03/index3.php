16c6d543c744c2da8419b5fd1975f464f779bac3
Daan Swart <dswart@roc-dev.com>
2020-10-15 22:22:13 +0200

    opdracht 1 t/m 3 klaar

---
+++ h03/index3.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="hs3-style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nova+Square&display=swap" rel="stylesheet">
+    <title>Hoofdstuk 2 php</title>
+</head>
+<body>
+<h1>Hoofdstuk 3</h1>
+<h3><a href="">Opdracht 1</a></h3>
+<h3><a href="">Opdracht 2</a></h3>
+<h3><a href="">Opdracht 3</a></h3>
+<h3><a href="">Opdracht 4</a></h3>
+<h3><a href="">Opdracht 5</a></h3>
+<h3><a href="">Opdracht 6</a></h3>
+<h3><a href="">Opdracht 7</a></h3>
+<h3><a href="">Opdracht 8</a></h3>
+<br>
+<a href="../index.php">Naar index van PHP voor beginners</a>
+</body>
+</html>
