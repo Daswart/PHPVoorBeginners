feb2471c0b37bdec4a7969e25382361d44f841b6
Daan Swart <dswart@roc-dev.com>
2020-10-21 12:47:44 +0200

    Klaar: opdracht 4 ("van een while naar een for-loop")

---
+++ h03/opdracht4.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>H3 opdracht 4</title>
+</head>
+<body>
+<?php
+
+//De for-loop
+for ($x = 35; $x >= 25; $x-- ){
+    echo "hoppelepee";
+}
+//Hetzelfde resultaat, maar dan met een While-loop
+/*$x = 35;
+while ($x >= 25) {
+    echo "hoppelepee ";
+    $x--;
+}*/
+
+?>
+
+</body>
+</html>
