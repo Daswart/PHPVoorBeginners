d1630284944dece7420c426a333fb92a8fff0a07
Daan Swart <dswart@roc-dev.com>
2020-10-22 20:59:35 +0200

    add H04/opdracht 3 "Celcius to Fahrenheit"

---
+++ h04/opdracht3.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>H4 opdracht 3</title>
+    <style></style>
+</head>
+<body>
+<?php
+echo reverseString("Daan");
+
+
+
+function reverseString($string){
+$stringlength = strlen($string);
+$reversedString = "";
+for($i = $stringlength - 1; $i >= 0; $i--){
+    $reversedString .= substr($string, $i, 1);
+
+}
+
+    return $reversedString;
+}
+
+?>
+
+</body>
+</html>
