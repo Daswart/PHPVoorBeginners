518513e667ed2acfa921309350ee2bd3c70d850d
Daan Swart <dswart@roc-dev.com>
2020-10-23 09:22:23 +0200

    added link to ../index.php

--- h04/index.php
+++ h04/index.php
@@ -5,6 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="hs4-style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nova+Square&display=swap" rel="stylesheet">
     <title>H4 PHP</title>
 </head>
 <body>
@@ -12,5 +14,6 @@
 <h3><a href="opdracht1.php">Opdracht 1</a></h3>
 <h3><a href="opdracht2.php">Opdracht 2</a></h3>
 <h3><a href="opdracht3.php">Opdracht 3</a></h3>
+<a href="../index.php">Naar index van PHP voor beginners</a>
 </body>
 </html>
