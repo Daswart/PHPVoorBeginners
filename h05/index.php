78a896d4f7b227133674a62b35b88560411ed317
Daan Swart <dswart@roc-dev.com>
2020-10-23 09:20:12 +0200

    added h05/index with hs5 css-file

---
+++ h05/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="hs5-style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nova+Square&display=swap" rel="stylesheet">
+    <title>H5 php</title>
+</head>
+<body>
+<h1>Hoofdstuk 5</h1>
+<h3><a href="">Opdracht 1</a></h3>
+<h3><a href="">Opdracht 2</a></h3>
+<h3><a href="">Opdracht 3</a></h3>
+<h3><a href="">Opdracht 4</a></h3>
+<h3><a href="">Opdracht 5</a></h3>
+<a href="../index.php">Naar index van PHP voor beginners</a>
+
+</body>
+</html>
