3001e642f71525d59fde55aaec251fc2c9b0db14
Daan Swart <dswart@roc-dev.com>
2020-10-23 09:46:17 +0200

    added link to form.html (opdracht 1)

--- h05/index.php
+++ h05/index.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 <h1>Hoofdstuk 5</h1>
-<h3><a href="">Opdracht 1</a></h3>
+<h3><a href="form.html">Opdracht 1</a></h3>
 <h3><a href="">Opdracht 2</a></h3>
 <h3><a href="">Opdracht 3</a></h3>
 <h3><a href="">Opdracht 4</a></h3>
