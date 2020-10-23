ae13ba596e770de84e6b2876ace6a43158a754f4
Daan Swart <dswart@roc-dev.com>
2020-10-23 15:20:41 +0200

    h05/resultaat-opdracht2.php changed if/else statement (shortened)

--- h05/resultaat-opdracht2.php
+++ h05/resultaat-opdracht2.php
@@ -1,29 +1,16 @@
 <?php
-if ($_POST["naam"] == "") {
-    echo 'je moet je naam nog invullen' . "<br>";
-    echo "<a href='opdracht2.html'>terug naar het formulier</a> <br>";
+if ($_POST["naam"] == "" || $_POST["adres"] == "" || $_POST["email"] == "" || $_POST["wachtwoord"] == "") {
+    echo '<script language="javascript">';
+    echo 'alert("er zijn 1 of meerdere velden niet ingevuld")';
+    echo '</script>';
+    echo "<a href='opdracht2.html'><< Ga terug naar het formulier</a> <br>";
 } else {
     echo $_POST["naam"] . "<br>";
-
-}
-
-if ($_POST["adres"] == "") {
-    echo 'je moet je adres nog invullen'. "<br>";
-    echo "<a href='opdracht2.html'>terug naar het formulier</a> <br>";
-} else {
     echo $_POST["adres"] . "<br>";
-
-}
-
-if ($_POST["email"] == "") {
-    echo 'je moet je email nog invullen' . "<br>";
-    echo "<a href='opdracht2.html'>terug naar het formulier</a> <br>";
-} else {
     echo $_POST["email"] . "<br>";
 
+
 }
-if ($_POST["wachtwoord"] == "") {
-    echo 'je moet je wachtwoord nog invullen' . "<br>";
-    echo "<a href='opdracht2.html'>terug naar het formulier</a> <br>";
-}
+
+
 
