76ae0e9465935751f6773eedc80e30a9cc3badd3
Daan Swart <dswart@roc-dev.com>
2020-10-23 09:12:50 +0200

    added link to h05/index

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
     <li><a id="h1" href="h01/index.php">Hoofdstuk 1</a></h3></Li>
     <li><a id="h2" href="h02/index2.php">Hoofdstuk 2</a></li>
     <li><a id="h3" href="h03/index3.php">Hoofdstuk 3</a></li>
-    <li><a href="">Hoofdstuk 4</a></li>
-    <li><a href="">Hoofdstuk 5</a></li>
+    <li><a href="h04/index.php">Hoofdstuk 4</a></li>
+    <li><a href="h05/index.php">Hoofdstuk 5</a></li>
     <li><a href="">Hoofdstuk 6</a></li>
     <li><a href="">Hoofdstuk 7</a></li>
 </ul>
