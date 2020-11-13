3fec24e61b6312b3324321d861bf923af50c0a43
Daan Swart <dswart@roc-dev.com>
2020-11-13 11:35:47 +0100

    added link tot h06/opdracht 2

--- h06/index.php
+++ h06/index.php
@@ -5,14 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="hs6-style.css">
+    <link rel="stylesheet" href="../h05/hs5-style.css">
     <link href="https://fonts.googleapis.com/css2?family=Nova+Square&display=swap" rel="stylesheet">
     <title>H6 php</title>
 </head>
 <body>
 <h1>Hoofdstuk 6</h1>
 <h3><a href="opdracht1.php">Opdracht 1</a></h3>
-<h3><a href=" ">Opdracht 2</a></h3>
+<h3><a href="opdracht2.php">Opdracht 2</a></h3>
 
 
 <a href="../index.php">Naar index van PHP voor beginners</a>
