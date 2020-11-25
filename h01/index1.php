4f04c0e64c94accaaaf88e438151afc7655172cf
Daan Swart <dswart@roc-dev.com>
2020-11-25 16:29:09 +0100

    added h01/index1.php

---
+++ h01/index1.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href=../h02/hs2-style.css>
+    <link href="https://fonts.googleapis.com/css2?family=Nova+Square&display=swap" rel="stylesheet">
+    <title>Hoofdstuk 1 php</title>
+</head>
+<body>
+<h1>Hoofdstuk 1</h1>
+<h3><a href="opdracht1.php">Opdracht 1</a></h3>
+
+<br>
+<a href="../index.php">Naar index van PHP voor beginners</a>
+</body>
+</html>
