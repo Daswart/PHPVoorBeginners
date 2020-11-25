96f704e0af800d4ad399c9a26420fb88f4f6d69b
Daan Swart <dswart@roc-dev.com>
2020-11-25 16:39:10 +0100

    added hs.style.css

--- h07/index.php
+++ h07/index.php
@@ -5,9 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../h05/hs5-style.css">
+    <link rel="stylesheet" href="../hs-style.css">
     <link href="https://fonts.googleapis.com/css2?family=Nova+Square&display=swap" rel="stylesheet">
-    <title>Hoofdstuk 6</title>
+    <title>Hoofdstuk 7 PHP</title>
 </head>
 <body><h1>Hoofdstuk 7</h1>
 
