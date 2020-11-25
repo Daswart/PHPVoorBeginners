3b5b9a7b4e21b2a2bdbaf666e5b0495ccc1ddade
Daan Swart <dswart@roc-dev.com>
2020-11-25 16:30:11 +0100

    renamed to opdracht1.php

--- h01/opdracht1.php
+++ h01/opdracht1.php
@@ -9,11 +9,12 @@ echo 'Hello world! First time working with php.';
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hoofdstuk 1 php</title>
+    <title>Opdracht 1 php</title>
 </head>
 <body>
 <br>
 <br>
-<a href="../index.php">Naar de index van PHP 2020</a>
+
+<a href="index1.php">Naar index van Hoofdstuk 1</a>
 </body>
 </html>
